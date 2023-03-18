0aeec7fd0bc075974fd2a20bc179ab8fca72bf9f
bhawani47 <120445101+bhawani47@users.noreply.github.com>
2023-03-18 17:49:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-const app = require('./app');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require('dotenv').config();
+require('./config');
 
-module.exports = (req, res) => {
-  app(req, res);
-};
+const app = express();
+
+// //  Parsing URL
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors({
+  credentials:true
+}));
+
+// //  Importing Routes
+const registerRoute = require("./routes/registerRoute");
+const sendOtpRoute  = require('./routes/sendotpRoute');
+const loginRoute = require('./routes/loginRoute.js');
+const authuserRoute  = require('./routes/authuserRoute');
+
+// //   Defining Routes
+app.get("/", (req, res) => { res.send("Hello World");});
+app.use('/api/register',registerRoute);
+app.use('/api/sendotp',sendOtpRoute);
+app.use('/api/login',loginRoute);
+app.use('/api/authuser',authuserRoute);
+
+app.listen(3000);
